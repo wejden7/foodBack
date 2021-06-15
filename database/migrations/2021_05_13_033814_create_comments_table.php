b0921f31e570f33b8bb78169f666ae15c00c4909
wejden <wejdenchneti7@gmail.om>
2021-06-15 14:43:04 +0100

    TOW

--- database/migrations/2021_05_13_033814_create_comments_table.php
+++ database/migrations/2021_05_13_033814_create_comments_table.php
@@ -16,7 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('comment');
-            $table->bigInteger("id_repondre")->>nullable()();
+            $table->bigInteger("id_repondre")->nullable()();
             $table->enum('type', array('comment', 'repondre',));
             $table->timestamps();
 
